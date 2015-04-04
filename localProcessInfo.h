5c64b098dd7a32418a0408bbe722677a6d7fdd49
AmbroAAA <guohongyi1991@gmail.com>
2015-04-03 23:17:26 -0500

    Update localProcessInfo.h

--- localProcessInfo.h
+++ localProcessInfo.h
@@ -1,1 +1,24 @@
-
+#include "messageFormat.h"
+#include <vector>
+class algorithmDataStruct
+{
+protected:
+  int quorumSize;
+  int totalNodeNumber;
+  
+public:
+  int nodeID;
+  vector<int> quorumMembers;
+  bool REQUESTING;
+  bool TOKEN;
+  bool USING;
+  bool KNOWFLAG;
+  int TADDR;      //save the nodeID of which hold the token
+  bool SERVFLAG;
+  int SERVEE;    //save the nodeID of which the local node is serving
+  
+  char **mapIDtoIP; //save the IP Address of correspond nodeID
+  
+  void receiveMessage(Packet msg);
+  void sendMessage(Packet msg);
+}
