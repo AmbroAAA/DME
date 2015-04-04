503d7fdf023dab9e0be6d6fa19e683b5414d25e7
AmbroAAA <guohongyi1991@gmail.com>
2015-04-03 22:56:14 -0500

    Update MessageFormat.h

--- MessageFormat.h
+++ MessageFormat.h
@@ -1,1 +1,22 @@
 #pragma once
+
+/*=======================Message Types========================*/
+#define REQUEST_FINAL 0
+#define REQUEST_PARTIAL 1
+#define HAVE_TOKEN 2
+#define RELEASE 3
+#define SEND_TOKEN 4
+#define RREQUEST 5
+#define SETTLE 6
+#define UPDATE_HOLD_TOKEN 7
+#define UPDATE_LOSE_TOKEN 8
+
+#define MAX_IP_LENGTH 16
+
+/*=====================Message Packet========================*/
+struct Packet
+{
+  int TYPE;              //Message Type
+  int senderID;          //This message`s senderID
+  
+}
